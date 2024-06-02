ae8df918fe874f39386bd43f2d030a4b18322f9c
Ferhat Akalan <102834897+FerhatAkalan@users.noreply.github.com>
2024-06-03 00:26:31 +0300

    update

--- index.js
+++ index.js
@@ -84,3 +84,17 @@ function getImageForRepo(repoName) {
     };
     return imageMap.hasOwnProperty(repoName) ? imageMap[repoName] : "./images/default.jpg";
 }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("backToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("backToTopBtn").style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
